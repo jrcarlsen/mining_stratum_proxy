ab48931371c1ed2deb792e1ee1bf402be2f1fe51
Johnny Rose Carlsen <jrcarlsen@gmail.com>
2018-01-03 16:16:24 +0700

    Basic proxying works now, but still no error handling

--- stratum_proxy.py
+++ stratum_proxy.py
@@ -15,31 +15,132 @@ BACKEND_PORT    = 6666
 
 ################################################################################
 
-class Client:
-    def __init__(self, accept):
-        self.socket, self.address = accept
-        self.connected = True
-        self.buffer = ''
+class Connection:
+    buffer_in = ''
+    buffer_out = ''
+
+    def __init__(self, client, socket_accept=None):
+        self.client = client
+        if socket_accept:
+            self.socket, self.addr = socket_accept
+            self.connected = True
+            print self, "CONNECTED"
+            self.socket_setmode('r')
+        else:
+            self.addr = (BACKEND_ADDRESS, BACKEND_PORT)
+            self.socket = None
+            self.connected = False
 
     def __repr__(self):
-        return "<Client fileno='%i', buffer_length='%i'>" % (self.socket.fileno(), len(self.buffer))
+        if self.socket:
+            fileno = self.socket.fileno()
+        else:
+            fileno = -1
+        return "<Connection fileno='%i' addr='%s' buffer_in='%i' buffer_out='%i'>" % (
+            fileno, self.addr, len(self.buffer_in), len(self.buffer_out))
 
+    def socket_setmode(self, mode):
+        print self, "SETMODE", mode
+        self.client.server.socket_setmode(self.socket, mode, self)
+
+    def disconnect(self):
+        print self, "DISCONNECT"
+        assert(self.connected == True)
+#        try:
+        self.socket_setmode(None)
+#        except socket.error:
+#            pass
+        self.connected = False
+        self.socket.close()
+        self.socket = None
+
+    def connect(self):
+        print self, "CONNECT"
+        assert(self.connected == False)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(self.addr)
+        self.socket_setmode('r')
+        self.connected = True        
+    
     def event(self, event):
-        assert(event == 1)
+        if event & select.EPOLLIN:
+            self.net_receive()
+        elif event & select.EPOLLOUT:
+            self.net_send()
+        else:
+            print "ERROR: Uknown event", self, event
+
+    def net_receive(self):
         data = self.socket.recv(4096)
         if not data:
-            self.connected = False
+            self.disconnect()
             return
 
-        self.buffer += data
-       
+        print self, "<<<", data
+        self.buffer_in += data
+        self.client.process_connections()
+
+    def net_send(self):
+        data = self.buffer_out[:4096]
+        self.buffer_out = self.buffer_out[4096:]
+        if not self.buffer_out:
+            self.socket_setmode('r')
+        if data:
+            print self, ">>>", data
+            self.socket.send(data)
         
+    def send(self, data):
+        self.buffer_out += data
+        if not self.connected:
+            self.connect()
+        self.socket_setmode('rw')
+
+    def parse_buffer(self):
+        lines = self.buffer_in.split('\n')
+        if len(lines) == 1:
+            return []
+        self.buffer_in = lines[-1]
+        return lines[:-1]
+
+################################################################################
+
+class Client:
+    def __init__(self, server):
+        self.server  = server
+        self.client  = Connection(self, server.socket.accept()) 
+        self.backend = Connection(self)
+
+    def __repr__(self):
+        if self.client.connected:
+            fileno = self.client.socket.fileno()
+        else:
+            fileno = -1
+        return "<Client>" # % (
+          #  fileno, len(self.client.buffer_in), len(self.backend.buffer_out))
+
+    def retired(self):
+        if self.backend.buffer_out != 0:
+            return False
+        return True
+
+    def process_connections(self):
+        lines = self.client.parse_buffer()
+        for line in lines:
+            self.backend.send(line+'\n')
         
+        lines = self.backend.parse_buffer()
+        for line in lines:
+            self.client.send(line+'\n')
+
+        if not self.backend.connected:
+            self.backend.connect()
 
 ################################################################################
 
 class Server:
     sockets = {}
+    clients = {}
+    connected = True
     epoll = select.epoll()
 
     def __init__(self, port):
@@ -51,44 +152,61 @@ class Server:
         self.socket.setblocking(0)
 
         # Register our socket
-        self.socket_register(self.socket, self)
-        #self.sockets[self.socket.fileno()] = self
-        #self.epoll.register(self.socket.fileno(), select.EPOLLIN)
+        self.socket_setmode(self.socket, 'r', self)
 
     def __repr__(self):
-        return "<Server fileno='%i', clients='%i'>" % (self.socket.fileno(), len(self.sockets)-1)
+        return "<Server fileno='%i', clients='%i'>" % (self.socket.fileno(), len(self.clients))
 
-    def socket_register(self, socket, obj):
-        self.epoll.register(socket.fileno(), select.EPOLLIN)
-        self.sockets[socket.fileno()] = obj
+    def socket_setmode(self, socket, mode, callback_obj):
+        if not mode:
+            del self.sockets[socket.fileno()]
+            self.epoll.unregister(socket.fileno())
+            return
 
-    def socket_unregister(self, socket):
-        assert(self.sockets.has_key(socket.fileno()) == True)
-        del self.sockets[socket.fileno()]
-        self.epoll.unregister(socket.fileno()) 
+        modes = {
+            'r': select.EPOLLIN,
+            'rw': select.EPOLLIN + select.EPOLLOUT,
+        }
 
-    def check_sockets(self, timeout):
+        if not self.sockets.has_key(socket.fileno()):
+            self.sockets[socket.fileno()] = callback_obj
+            self.epoll.register(socket.fileno(), modes[mode])
+        else:
+            self.sockets[socket.fileno()] = callback_obj
+            self.epoll.modify(socket.fileno(), modes[mode])
+
+    def sockets_poll(self, timeout):
         events = self.epoll.poll(timeout)
         for fileno, event in events:
+            #print ">", fileno, event
             assert(self.sockets.has_key(fileno) == True)
             self.sockets[fileno].event(event)
 
+    def clients_cleanup(self):
+        for client in self.clients:
+            if client.retired():
+                print client, "RETIRED"
+                del self.clients[client]
+
     def event(self, event):
         assert(event == 1)
-        print "event"
-        client = Client(self.socket.accept())
-        self.socket_register(client.socket, client)
+        #print "got event"
+        client = Client(self)
+        self.clients[client] = True
 
     def status(self):
-        for s in self.sockets.values():
-            print "!", s
-        print
+        for c in self.clients:
+            print "[32mC ", c
+            print " => client:  ", c.client
+            print " => backend: ", c.backend
+        print "[0m"
 
 ################################################################################
 
 s = Server(PORT)
 
 while True:
-    s.check_sockets(timeout=3)
+    s.sockets_poll(timeout=5)
+    s.clients_cleanup()
     s.status()
 
