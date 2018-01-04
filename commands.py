3d4926d02a612348536cb6946e932b0ea6ec820b
Johnny Carlsen <jrcarlsen+github@gmail.com>
2018-01-04 16:16:21 +0700

    Added noop command

--- commands.py
+++ commands.py
@@ -1,10 +1,24 @@
-def cmd_connections(client, cmd):
-    client.client.send('connected clients: %i\n' % len(client.server.clients))
+################################################################################
 
-def cmd_quit(client, cmd):
-    client.client.disconnect()
+class Monitor:
+    def cmd_connections(client, cmd):
+        client.client.send('connected clients: %i\n' % len(client.server.clients))
+
+    def cmd_quit(client, cmd):
+        client.client.disconnect()
+
+    def cmd_noop(self, client, cmd):
+        return
+
+################################################################################
+
+m = Monitor()
 
 cmd_map = {
-    'connections':  cmd_connections,
-    'quit':         cmd_quit,
+    'connections':  m.cmd_connections,
+    'quit':         m.cmd_quit,
+    '':             m.cmd_noop,
 }
+
+################################################################################
+
