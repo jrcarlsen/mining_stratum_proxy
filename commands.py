26da6a19d9e1fea78ae63149eae538df96fa456e
Johnny Carlsen <jrcarlsen+github@gmail.com>
2018-01-04 16:10:06 +0700

    Added quit command

--- commands.py
+++ commands.py
@@ -1,6 +1,10 @@
 def cmd_connections(client, cmd):
     client.client.send('connected clients: %i\n' % len(client.server.clients))
 
+def cmd_quit(client, cmd):
+    client.client.disconnect()
+
 cmd_map = {
-    'connections': cmd_connections,
+    'connections':  cmd_connections,
+    'quit':         cmd_quit,
 }
