91646c43b35141c1486c5b09b8c0244da7513a7d
Johnny Carlsen <jrcarlsen+github@gmail.com>
2018-01-04 16:33:21 +0700

    ...

--- commands.py
+++ commands.py
@@ -13,6 +13,13 @@ class Monitor:
     def cmd_noop(self, client, cmd):
         return
 
+    def cmd_status(self, client, cmd):
+        mesg = "PSProxy Status:\n"
+        mesg += "Bytes in: 0\n"
+        mesg += "Bytes out: 0\n"
+        mesg += "Connections: %i\n" % len(client.server.clients)
+        client.client.send(mesg)
+
 ################################################################################
 
 m = Monitor()
