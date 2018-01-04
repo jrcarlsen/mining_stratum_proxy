6eb4d274756313f2fef91e3c155434674502a1fd
Johnny Carlsen <jrcarlsen+github@gmail.com>
2018-01-04 16:06:10 +0700

    Moved commands to its own file

--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
 def cmd_connections(client, cmd):
-    client.send('connected clients: %i\n' % len(client.server.clients))
+    client.client.send('connected clients: %i\n' % len(client.server.clients))
 
 cmd_map = {
     'connections': cmd_connections,
